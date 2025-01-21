efe212f1d286185d2a83166c03f60e3f2de7816d
hastiatarod <h.atarod.h@gmail.com>
2025-01-21 19:26:18 +0330

    search Engine

---
+++ src/searchEngine.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SearchEngine(){
+    return(
+        <div>
+            <form className="searchContainer">
+                <input type="text" placeholder="Enter a city.."></input>
+                <button type="submit"> search </button>
+            </form>
+        </div>
+    )
+}
